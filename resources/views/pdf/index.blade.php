6b1e5d07a5bb1f8df136e875eed77eb9a5ffc06a
PatrickNthiwa <pwnthiwa@gmail.com>
2023-05-08 10:59:37 +0300

    "\r\n"

--- resources/views/pdf/index.blade.php
+++ resources/views/pdf/index.blade.php
@@ -3,14 +3,16 @@
 @section('content')
 
 <div class='container'>
- @if($errors->any())
- <div class="alert alert-danger">
-   <ul>
-   @foreach($errors->all() as $error)
-   <li>{{$error}}</li>
-   $endforeach
-   </ul>
- </div>
- @endif
+    @if($errors->any())
+        <div class="alert alert-danger">
+        <ul>
+                @foreach($errors->all() as $error)
+                <li>{{$error}}</li>
+                $endforeach
+        </ul>
+        </div>
+    @endif
 </div>
+
+
 @endsection

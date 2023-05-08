1a169efdf88495fee3fd1aeaeb76a989dfc0d802
PatrickNthiwa <pwnthiwa@gmail.com>
2023-05-08 10:57:20 +0300

    "f"

---
+++ resources/views/pdf/index.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class='container'>
+ @if($errors->any())
+ <div class="alert alert-danger">
+    @foreach
+ </div>
+ @endif
+</div>
+@endsection

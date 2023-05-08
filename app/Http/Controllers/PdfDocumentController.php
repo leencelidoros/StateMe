6752f5f6d4e74ee3043ce0ff37252ddf05ca0152
PatrickNthiwa <pwnthiwa@gmail.com>
2023-05-08 13:44:51 +0300

    "    " "    " "        " "    " "        " "    " "        " "        " "        " "        " "    " "        " "        " "            " "                " "            " "                " "            " "        " "        " "        " "        " "        " "        " "    " "        " "            " "        " "    " "    "

--- app/Http/Controllers/PdfDocumentController.php
+++ app/Http/Controllers/PdfDocumentController.php
@@ -13,4 +13,34 @@ class PdfDocumentController extends Controller
     public function index(){
        return view('pdf.index');
     }
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'pdf_file' => 'required|mimes:pdf|max:2048'
+        ]);
+
+        $pdf = $request->file('pdf_file');
+        $pdfPath = $pdf->getPathname();
+        $pdftk = new Pdftk($pdfPath);
+        $dataFields = $pdftk->getDataFields();
+        $text = '';
+        foreach ($dataFields as $fieldName => $fieldValue) {
+            if ($fieldName == 'Metadata') {
+                $text .= $fieldValue;
+            } else {
+                $text .= "\n" . $fieldName . ': ' . $fieldValue;
+            }
+        }
+        Log::debug("Extracted text : ",[$text]);
+
+        $pdfDocument = new PdfDocument;
+        $pdfDocument->title = $pdf->getClientOriginalName();
+        $pdfDocument->content =$text ;
+        $pdfDocument->save();
+
+        $pdfPath = $pdf->store('pdf', 'public');
+
+        return redirect()->back()->with('success', 'PDF uploaded successfully')->with('pdf', asset('storage/'.$pdfPath));
+    }
+
 }

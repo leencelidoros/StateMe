3d0581cf9e58b41b27f168b4a95f4fdcbe2fc5d7
PatrickNthiwa <pwnthiwa@gmail.com>
2023-05-08 11:51:38 +0300

    "r"

--- app/Http/Controllers/PdfDocumentController.php
+++ app/Http/Controllers/PdfDocumentController.php
@@ -23,7 +23,7 @@ class PdfDocumentController extends Controller
 
         if($conv_contents == false){
             \Log::error('Failed to convert into UTF_8 encoding');
-            ret
+            return ->respo
         }
 
         try {

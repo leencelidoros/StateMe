ddba8d18ddaada0f1d9e97668d12aa737c04775d
PatrickNthiwa <pwnthiwa@gmail.com>
2023-05-08 10:48:55 +0300

    "\n        "

--- app/Http/Controllers/PdfDocumentController.php
+++ app/Http/Controllers/PdfDocumentController.php
@@ -21,6 +21,7 @@ class PdfDocumentController extends Controller
 
         $pdf_document = new PdfDocument();
         $pdf_document ->title= file_get_contents($pdf->getClientOriginalName());
-        $pdf_document->content=$ontents
+        $pdf_document->content=$contents;
+        
     }
 }

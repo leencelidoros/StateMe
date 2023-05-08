9822632649dc6c8fe4a516ca41d0295cf901e200
PatrickNthiwa <pwnthiwa@gmail.com>
2023-05-08 10:47:42 +0300

    "\n        "

--- app/Http/Controllers/PdfDocumentController.php
+++ app/Http/Controllers/PdfDocumentController.php
@@ -19,6 +19,7 @@ class PdfDocumentController extends Controller
         $pdf=$request->file('pdf_file');
         $contents=file_get_contents($pdf->getRealPath());
 
-        $pdf_document = new PdfDocument()
+        $pdf_document = new PdfDocument();
+        
     }
 }

ac1b114f91f50d4394717cc3352ae7f43fa88aa8
PatrickNthiwa <pwnthiwa@gmail.com>
2023-05-08 10:44:50 +0300

    "f"

--- app/Http/Controllers/PdfDocumentController.php
+++ app/Http/Controllers/PdfDocumentController.php
@@ -15,6 +15,6 @@ class PdfDocumentController extends Controller
            [ 'pdf_file'=>'required|mimes:pdf|max:2048']
         );
 
-        $request->file('pdf_file')
+        $f=$request->file('pdf_file');
     }
 }

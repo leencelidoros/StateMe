3108a2344b65c18090cdb90d59314f54332da39b
PatrickNthiwa <pwnthiwa@gmail.com>
2023-05-08 10:50:16 +0300

    " "

--- app/Http/Controllers/PdfDocumentController.php
+++ app/Http/Controllers/PdfDocumentController.php
@@ -25,6 +25,6 @@ class PdfDocumentController extends Controller
         $pdf_document->save();
 
 
-        return redirect()->back()->with('success','pdf ')
+        return redirect()->back()->with('success','pdf uploaded succesf')
     }
 }

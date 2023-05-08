3f06141f54dc7bb2d4fb099e756a377769285262
PatrickNthiwa <pwnthiwa@gmail.com>
2023-05-08 14:55:09 +0300

    ";"

--- app/Http/Controllers/PdfDocumentController.php
+++ app/Http/Controllers/PdfDocumentController.php
@@ -10,7 +10,7 @@ use Symfony\Component\HttpFoundation\File\UploadedFile;
 
 class PdfDocumentController extends Controller
 {
-  private 
+  private $pdf;
     public function index(){
        return view('pdf.index');
     }

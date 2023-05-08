c96d2362b48cabe82897e35078254316624225c1
PatrickNthiwa <pwnthiwa@gmail.com>
2023-05-08 12:42:43 +0300

    "g"

--- app/Http/Controllers/PdfDocumentController.php
+++ app/Http/Controllers/PdfDocumentController.php
@@ -20,7 +20,7 @@ class PdfDocumentController extends Controller
         $pdf = $request->file('pdf_file');
 
         $text = shell_exec("pdftotext {$pdf->getRealPath()} -");
-        Log::debug
+        Log::debug("Ex")
 
         $pdfDocument = new PdfDocument;
         $pdfDocument->title = $pdf->getClientOriginalName();

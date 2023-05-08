158e61ff700bcfb13aa82941507cb757813b2e90
PatrickNthiwa <pwnthiwa@gmail.com>
2023-05-08 11:35:09 +0300

    ""

--- app/Http/Controllers/PdfDocumentController.php
+++ app/Http/Controllers/PdfDocumentController.php
@@ -19,15 +19,11 @@ class PdfDocumentController extends Controller
         $pdf=$request->file('pdf_file');
         $contents=file_get_contents($pdf->getRealPath());
 
-        // $pdf_document = new PdfDocument();
-        // $pdf_document ->title= $pdf->getClientOriginalName();
-        // $pdf_document->content=$contents;
-        // $pdf_document->save();
-        $pdfDocument = PdfDocument::create([
-            'title' => $pdf->getClientOriginalName(),
-            'content' => $contents
-        ]);
-        
+        $pdf_document = new PdfDocument();
+        $pdf_document ->title= $pdf->getClientOriginalName();
+        $pdf_document->content=$contents;
+        $pdf_document->save();
+
 
         return redirect()->back()->with('success','pdf uploaded succesfully');
     }

4efa6adfa40d53767e915a3197b7881202831479
PatrickNthiwa <pwnthiwa@gmail.com>
2023-05-08 11:07:39 +0300

    "t"

--- resources/views/pdf/index.blade.php
+++ resources/views/pdf/index.blade.php
@@ -24,7 +24,7 @@
     <form action="{{route('pdf.store')}}" method="POST" enctype="multipart/form-data">
         @csrf
         <div class="form-group">
-            <label for="pdf_file"></label>
+            <label for="pdf_file">Select </label>
         </div>
         <button type="submit" class="btn btn-primary">Upload</button>
     </form>

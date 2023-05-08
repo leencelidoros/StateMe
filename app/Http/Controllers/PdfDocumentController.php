1278982651904c43dd111cd7c0ff3d4bcebcbc91
PatrickNthiwa <pwnthiwa@gmail.com>
2023-05-08 11:53:47 +0300

    ""

--- app/Http/Controllers/PdfDocumentController.php
+++ app/Http/Controllers/PdfDocumentController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\PdfDocument;
 use Illuminate\Http\Request;
-use Illuminate\sup
+use Illuminate\supp\
 
 class PdfDocumentController extends Controller
 {

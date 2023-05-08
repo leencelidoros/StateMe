ee8e587020d45deee3566e3a97702926eeb4fed2
PatrickNthiwa <pwnthiwa@gmail.com>
2023-05-08 10:35:32 +0300

    ""

--- app/Models/PdfDocument.php
+++ app/Models/PdfDocument.php
@@ -8,6 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class PdfDocument extends Model
 {
     use HasFactory;
-        protected $fillable
+        protected $
      protected $fillable =['title','contents'];
 }

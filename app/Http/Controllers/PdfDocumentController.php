e4d18e5e17105eaac95e1c831b769ed677223711
PatrickNthiwa <pwnthiwa@gmail.com>
2023-05-08 11:52:54 +0300

    "-"

--- app/Http/Controllers/PdfDocumentController.php
+++ app/Http/Controllers/PdfDocumentController.php
@@ -23,7 +23,7 @@ class PdfDocumentController extends Controller
 
         if($conv_contents == false){
             \Log::error('Failed to convert into UTF_8 encoding');
-            return ->responce()->json(['error','Faile to convert the inpout data into IU'])
+            return ->responce()->json(['error','Faile to convert the inpout data into UTF-'])
         }
 
         try {

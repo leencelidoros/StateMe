5aaef8c6e08629a9efb04594e38db9bdfbf3ab68
PatrickNthiwa <pwnthiwa@gmail.com>
2023-05-08 13:43:50 +0300

    "public function store(Request $request)\n{\n    $validated = $request->validate([\n        'pdf_file' => 'required|mimes:pdf|max:2048'\n    ]);\n\n    $pdf = $request->file('pdf_file');\n    $pdfPath = $pdf->getPathname();\n    $pdftk = new Pdftk($pdfPath);\n    $dataFields = $pdftk->getDataFields();\n    $text = '';\n    foreach ($dataFields as $fieldName => $fieldValue) {\n        if ($fieldName == 'Metadata') {\n            $text .= $fieldValue;\n        } else {\n            $text .= \"\\n\" . $fieldName . ': ' . $fieldValue;\n        }\n    }\n    Log::debug(\"Extracted text : \",[$text]);\n\n    $pdfDocument = new PdfDocument;\n    $pdfDocument->title = $pdf->getClientOriginalName();\n    $pdfDocument->content =$text ;\n    $pdfDocument->save();\n\n    $pdfPath = $pdf->store('pdf', 'public');\n\n    return redirect()->back()->with('success', 'PDF uploaded successfully')->with('pdf', asset('storage/'.$pdfPath));\n}\n"

--- app/Http/Controllers/PdfDocumentController.php
+++ app/Http/Controllers/PdfDocumentController.php
@@ -5,30 +5,62 @@ namespace App\Http\Controllers;
 use App\Models\PdfDocument;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
-use Spatie\PdfToText\Pdf;
+use mikehaertl\pdftk\Pdf as Pdftk;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 
 class PdfDocumentController extends Controller
 {
     public function index(){
        return view('pdf.index');
     }
+
     public function store(Request $request)
     {
         $validated = $request->validate([
             'pdf_file' => 'required|mimes:pdf|max:2048'
         ]);
-    
+
         $pdf = $request->file('pdf_file');
-        $text = Pdf::getText($pdf->getRealPath());
+        $pdfPath = $pdf->getPathname();
+        $pdftk = new Pdftk($pdfPath);
+        $dataFields = $pdftk->getDataFields();
+        $text = '';
+        foreach ($dataFields as $fieldName => $fieldValue) {
+            if ($fieldName == 'Metadata') {
+                $text .= $fieldValue;
+            } else {
+                $text .= "\n" . $fieldName . ': ' . $fieldValue;
+            }
+        }
         Log::debug("Extracted text : ",[$text]);
 
         $pdfDocument = new PdfDocument;
         $pdfDocument->title = $pdf->getClientOriginalName();
         $pdfDocument->content =$text ;
         $pdfDocument->save();
-    
+
         $pdfPath = $pdf->store('pdf', 'public');
-    
+
+        return redirect()->back()->with('success', 'PDF uploaded successfully')->with('pdf', asset('storage/'.$pdfPath));
+    }
+
+        $validated = $request->validate([
+            'pdf_file' => 'required|mimes:pdf|max:2048'
+        ]);
+
+        $pdf = $request->file('pdf_file');
+        $pdfPath = $pdf->getPathname();
+        $pdftk = new Pdftk($pdfPath);
+        $text = $pdftk->getPages();
+        Log::debug("Extracted text : ",[$text]);
+
+        $pdfDocument = new PdfDocument;
+        $pdfDocument->title = $pdf->getClientOriginalName();
+        $pdfDocument->content =$text ;
+        $pdfDocument->save();
+
+        $pdfPath = $pdf->store('pdf', 'public');
+
         return redirect()->back()->with('success', 'PDF uploaded successfully')->with('pdf', asset('storage/'.$pdfPath));
     }
 }

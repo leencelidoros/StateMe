f5d7967ac64f21f08030f4159d1fc97f08d9ef7b
PatrickNthiwa <pwnthiwa@gmail.com>
2023-05-08 13:52:37 +0300

    ""

--- app/Http/Controllers/PdfDocumentController.php
+++ app/Http/Controllers/PdfDocumentController.php
@@ -25,7 +25,7 @@ class PdfDocumentController extends Controller
         $dataFields = $pdftk->getDataFields();
 
         if(is_array($dataFields)){
-            else$dataFields
+            else
         }
         $text = '';
         foreach ($dataFields as $fieldName => $fieldValue) {

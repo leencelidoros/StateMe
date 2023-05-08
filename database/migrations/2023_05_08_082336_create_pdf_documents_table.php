90a6aca2d7a4d53eb4aadc4f370356691b656258
PatrickNthiwa <pwnthiwa@gmail.com>
2023-05-08 11:24:23 +0300

    "t"

--- database/migrations/2023_05_08_082336_create_pdf_documents_table.php
+++ database/migrations/2023_05_08_082336_create_pdf_documents_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('pdf_documents', function (Blueprint $table) {
             $table->id();
-            $table->string()
+            $table->string('titk')
             $table->timestamps();
         });
     }

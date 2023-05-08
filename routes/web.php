9ba466d7fac3c1e71c5bc2136e19a58842eddccd
PatrickNthiwa <pwnthiwa@gmail.com>
2023-05-08 11:12:12 +0300

    "u"

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Illuminate\Http\Controll
+use Illuminate\Http\Controller\Au
 
 /*
 |--------------------------------------------------------------------------

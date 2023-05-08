6807bb62ff77ddd96921498b869f8e6bfc0d416b
PatrickNthiwa <pwnthiwa@gmail.com>
2023-05-08 11:11:00 +0300

    "e"

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use 
 
 /*
 |--------------------------------------------------------------------------
@@ -13,9 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 Route::get('/client',[\App\Http\Controllers\ClientController::class,'index'])->name('index');
 Route::get('/listclient',[\App\Http\Controllers\ClientController::class,'clientListing'])->name('show');

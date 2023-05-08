c995887174069630e5037b8572182ef82c0cc2c2
PatrickNthiwa <pwnthiwa@gmail.com>
2023-05-08 11:00:13 +0300

    "c"

---
+++ resources/views/pdf/index.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class='container'>
+    @if($errors->any())
+        <div class="alert alert-danger">
+        <ul>
+                @foreach($errors->all() as $error)
+                <li>{{$error}}</li>
+                $endforeach
+        </ul>
+        </div>
+    @endif
+</div>
+
+$if(session('success'))
+@endsection
